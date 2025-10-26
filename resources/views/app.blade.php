ac1a065ba55af876a971a009cf57bab521830db4
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2025-10-26 15:57:21 +0800

    deploy 5

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -28,7 +28,7 @@
             html.dark {
                 background-color: oklch(0.145 0 0);
             }
-                //
+                
         </style> --}}
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
