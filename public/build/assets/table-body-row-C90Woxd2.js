089cca4d6f5a9b1fb4899ab1384cd110d714b2e7
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2026-01-09 11:55:09 +0800

    build

---
+++ public/build/assets/table-body-row-C90Woxd2.js
@@ -0,0 +1,1 @@
+import{c as r,j as a}from"./app-4N8doYV2.js";import{a as i}from"./table-Bj2ZDPgF.js";function m(e){const o=r.c(2),{children:s}=e;let t;return o[0]!==s?(t=a.jsx(i,{className:"hover:bg-muted/50 transition-colors",children:s}),o[0]=s,o[1]=t):t=o[1],t}export{m as T};
